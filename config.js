46dd14dbe39a81975f4e2b70e892ba392202aded
Mamut880 <112500060+Mamut880@users.noreply.github.com>
2022-09-29 11:29:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
+global.owner = ['213676958744']
+global.ownernomer = "213676958744"
+global.premium = ['213676958744']
+global.packname = 'Rukbat'
 global.author = 'Naze'
 global.sessionName = 'nazedev'
 global.prefa = ['','!','.','#','&']
